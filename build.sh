1d76c655bcc1c5a869b9e61c72b9cb2e2dd57f89
luoxuzhi <herolxz@163.com>
2020-08-10 16:18:14 +0800

    change config

--- build.sh
+++ build.sh
@@ -16,6 +16,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:luoxuzhi/knowledge.git master:gh-pages
+git push -f git@github.com:luoxuzhi/know.git master:gh-pages
 
 cd -
